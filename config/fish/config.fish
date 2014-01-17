f3356de6cdae900ea6b78c8ac29c72c4952e8f88
Jack Bracewell <jbtwentythree@gmail.com>
2014-01-17 15:24:24 +0000

    Fish prompt nicked from Steve Losh
    https://bitbucket.org/sjl/dotfiles/src/tip/fish/config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,23 +3,67 @@ set fish_theme fishface
 set fish_plugins git
 
 
+#function fish_prompt
+#  function _git_branch_name
+#    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+#  end
+
+#  function _is_git_dirty
+#    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+#  end
+
+#  set -l blue (set_color -o blue)
+#  set -l green (set_color -o green)
+
+#if [ (_git_branch_name) ]
+#    echo -n -s "$green><(((\"> "
+#  else
+#   echo -n -s "$blue><(((\"> "
+#end
+#end
+
+function git_prompt
+    if git root >/dev/null 2>&1
+        set_color normal
+        printf ' on '
+        set_color magenta
+        printf '%s' (git rev-parse --abbrev-ref HEAD)
+        set_color normal
+    end
+end
+
 function fish_prompt
-  function _git_branch_name
-    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-  end
+    set last_status $status
 
-  function _is_git_dirty
-    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-  end
+    echo
 
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
+    set_color magenta
+    printf '%s' (whoami)
+    set_color normal
+    printf ' at '
 
-  if [ (_git_branch_name) ]
-    echo -n -s "$green><(((\"> "
-  else
-    echo -n -s "$blue><(((\"> "
-  end
+    set_color yellow
+    printf '%s' (hostname|cut -d . -f 1)
+    set_color normal
+    printf ' in '
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    git_prompt
+
+    echo
+
+    if test $last_status -eq 0
+        set_color white -o
+        printf '><((°> '
+    else
+        set_color red -o
+        printf '[%d] ><((ˣ> ' $last_status
+    end
+
+    set_color normal
 end
 
 alias 'gst' 'git status '
